53733d5a4c54b4d6ee1650c3d7f300b720ff32e4
anna mendoza <anna@curvecommunications.biz>
2013-08-19 11:37:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoScaffoldingApp::Application.config.secret_token = 'd46e51f6eff5a5d204960a5353ce35c72bf238e164b2ffaad1a91b8f5e4fb008651a2ffdf93803534729fcfcbfde9a3265ee079ffba2516852d262684244388e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DemoScaffoldingApp::Application.config.session_store :cookie_store, key: '_demo-scaffolding-app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DemoScaffoldingApp::Application.config.session_store :active_record_store
